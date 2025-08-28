1023afd82b27a6d11570c8b27d68650bbb0694b1
Muhammad Zeeshan <ZeeshanZahid663@gmail.com>
2025-08-28 23:17:59 +0500

    blog page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/context/authContext";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,9 +24,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+          <Footer />
+        </AuthProvider>{" "}
       </body>
-    </html>
+    </html> 
   );
 }
---
+++ src/app/resources/guide-templates/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuideTemplate = () => {
+  return (
+    <>
+      <h1>Guide Template Page</h1>
+    </>
+  );
+};
+
+export default GuideTemplate;
